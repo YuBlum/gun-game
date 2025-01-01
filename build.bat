d0070b24e9c21b0506c473cb20260f97ab3302c0
blum <yuuybonito@gmail.com>
2025-01-01 15:05:47 -0300

    backbuffer

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 set OUT=game
 set SRCS=..\src\main.cpp ..\src\window.cpp
-set LIBS=Kernel32.lib User32.lib
+set LIBS=Kernel32.lib User32.lib Gdi32.lib
 
 pushd bin
 cl -Zi -W4 -WX -GS- -Gs9999999 -Fe:%OUT% %SRCS% %LIBS% -link -entry:main -nodefaultlib -subsystem:windows -stack:0x100000,0x100000
--- src/window.cpp
+++ src/window.cpp
@@ -4,9 +4,13 @@
 struct Window {
   HWND handle;
   bool is_running;
+  HDC backbuffer_dc;
+  BITMAPINFO backbuffer_info;
+  HBITMAP backbuffer_bitmap;
+  u32 *backbuffer_pixels;
 };
 
-static Window g_window;
+static Window g_window = {};
 
 static void
 crash(char *err) {
@@ -15,18 +19,38 @@ crash(char *err) {
 }
 
 static LRESULT
-window_procedure(HWND win, UINT msg, WPARAM wparam, LPARAM lparam) {
+window_procedure(HWND window, UINT msg, WPARAM wparam, LPARAM lparam) {
+  LRESULT result = 0;
   switch (msg) {
   case WM_DESTROY:
   case WM_QUIT:
     g_window.is_running = false;
     break;
+  case WM_PAINT:
+  {
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(window, &ps);
+    BitBlt(
+      hdc,
+      ps.rcPaint.left, ps.rcPaint.top,
+      ps.rcPaint.right - ps.rcPaint.left,
+      ps.rcPaint.bottom - ps.rcPaint.top,
+      g_window.backbuffer_dc,
+      0, 0,
+      SRCCOPY
+    );
+    EndPaint(window, &ps);
+  } break;
+  default:
+    result = DefWindowProcA(window, msg, wparam, lparam);
+    break;
   }
-  return DefWindowProcA(win, msg, wparam, lparam);
+  return result;
 }
 
 void
 make_window(void) {
+  /* create window */
   WNDCLASSA window_class = {};
   window_class.style = CS_HREDRAW|CS_VREDRAW;
   window_class.lpfnWndProc = window_procedure;
@@ -62,6 +86,24 @@ make_window(void) {
   if (!g_window.handle) crash("CreateWindowA failed.");
   ShowWindow(g_window.handle, SW_SHOW);
   g_window.is_running = true;
+  /* create backbuffer */
+  g_window.backbuffer_dc = CreateCompatibleDC(0);
+  g_window.backbuffer_info.bmiHeader.biSize = sizeof (BITMAPINFOHEADER);
+  g_window.backbuffer_info.bmiHeader.biWidth = WINDOW_W;
+  g_window.backbuffer_info.bmiHeader.biHeight = -WINDOW_H;
+  g_window.backbuffer_info.bmiHeader.biPlanes = 1;
+  g_window.backbuffer_info.bmiHeader.biBitCount = 32;
+  g_window.backbuffer_info.bmiHeader.biCompression = BI_RGB;
+  g_window.backbuffer_bitmap = CreateDIBSection(
+    g_window.backbuffer_dc,
+    &g_window.backbuffer_info,
+    DIB_RGB_COLORS,
+    (void **)&g_window.backbuffer_pixels,
+    0,
+    0
+  );
+  if (!g_window.backbuffer_bitmap || !g_window.backbuffer_pixels) crash("CreateDIBSection failed.");
+  SelectObject(g_window.backbuffer_dc, g_window.backbuffer_bitmap);
 }
 
 bool window_is_running(void) { return g_window.is_running; }
@@ -69,6 +111,7 @@ bool window_is_running(void) { return g_window.is_running; }
 void
 frame_begin(void) {
   MSG msg;
+  if (!InvalidateRect(g_window.handle, 0, false)) crash("InvalidateRect failed.");
   while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE)) {
     TranslateMessage(&msg);
     DispatchMessage(&msg);
@@ -77,4 +120,5 @@ frame_begin(void) {
 
 void
 frame_end(void) {
+  for (u32 i = 0; i < WINDOW_W * WINDOW_H; i++) g_window.backbuffer_pixels[i] = 0xcc3333;
 }
